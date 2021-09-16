7918790ee52cb04e5ea5d97cbdadac35229f95f2
yuchi0307 <gelee0307@gmail.com>
2021-09-16 22:27:24 +0800

    first commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect, useReducer } from "react";
+
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+//reducer function 可以寫在 component 之外，因為他與組建內的東西無關
+//而任何在 reducer function 內的值可以被 React 自動帶入
+const emailReducer = (state, action) => {
+  if(action.type==="USER_INPUT"){
+    return { value: action.val, isValid: action.val.includes('@') }
+  }
+  if(action.type==="INPUT_BLUR"){
+    return { value: state.value, isValid: state.value.includes('@') }
+  }
+  return { value:'', isValid:false }
+}
+const passwordReducer = (state, aciton) =>{
+  if(aciton.type==='USER_PSW')
+  {
+    return {value: aciton.val, isValid: aciton.val.trim().length > 6}
+  }
+  if(aciton.type==='USER_BLUR_PSW')
+  {
+    return {value: state.value, isValid: state.value.trim().length > 6}
+  }
+  return {value:'', isValid: false}
+}
+const Login = (props) => {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const[emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: undefined
+  })
+
+  const [ passwordState, dispatchPassword ] =useReducer(passwordReducer,
+    {value:'',
+    isValid: undefined
+   }
+    )
+
+  // useEffect(() => {
+    
+  //   const identifier = setTimeout(()=>{ 
+  //     console.log("validate form");
+  //     emailState.isValid(emailState.value.includes("@") && enteredPassword.trim().length > 6
+  //   );
+  // },500);
+  //  return ()=>{
+  //    console.log("CLEAN UP");
+  //    clearTimeout(identifier);
+  //  }
+  // }, [enteredEmail, enteredPassword]);
+
+  const emailChangeHandler = (event) => {
+    dispatchEmail({type:'USER_INPUT', val: event.target.value})
+
+    setFormIsValid(
+      event.target.value.includes('@')  && passwordState.isValid
+    );
+  };
+
+  const passwordChangeHandler = (event) => {
+    // setEnteredPassword(passwordState.value);
+    dispatchPassword({type: 'USER_PSW', val: event.target.value})
+
+    setFormIsValid(
+      event.target.value.trim().length > 6 && emailState.isValid
+    );
+  };
+
+  const validateEmailHandler = () => {
+    dispatchEmail({type: 'INPUT_BLUR'})
+  };
+
+  const validatePasswordHandler = () => {
+    // setPasswordIsValid(passwordState.value.trim().length > 6);
+    dispatchPassword({type:'USER_BLUR_PSW'})
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(emailState.value, passwordState.value);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailState.isValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={emailState.value}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordState.isValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={passwordState.value}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
